56ab5b10bea0de827ed6fbf5fafc1d806f0b416c
mysterymethod <chandra.pranay@yahoo.in>
2020-09-14 21:26:43 +0530

    V-16 Complete

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,9 +1,31 @@
 import React from 'react';
 
-const buildControl = (props) => {
-    return (
+import classes from './BuildControls.module.css';
+import BuildControl from './BuildControl/BuildControl';
 
+
+
+const controls = [
+    {label: 'Salad', type: 'salad'},
+    {label: 'Bacon', type: 'bacon'},
+    {label: 'Cheese', type: 'cheese'},
+    {label: 'Meat', type: 'meat'}
+];
+
+const buildControls = (props) => {
+    return (
+        <div className = {classes.BuildControls}>
+            {controls.map(ctrl => (
+                <BuildControl 
+                    key = {ctrl.label} 
+                    label = {ctrl.label}
+                />
+            ))}
+        </div>
     );
 }
 
-export default buildControl;
+export default buildControls;
+
+
+   
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,25 +2,38 @@ import React, { Component } from 'react';
 
 import Aux from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
 
 
     state = {
         ingredients: {
-            salad: 0,
-            bacon: 0,
-            cheese: 10,
-            meat: 0 
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
         }
     }
 
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCounted = oldCount + 1;
+    }
+
+    removeIngredientHandler = (type) => {
+
+    }
 
     render () {
         return (
             <Aux>
                 <Burger 
                     ingredients = {this.state.ingredients}
+                />
+
+                <BuildControls 
+
                 />
                 <div> Build Control </div>
             </Aux>
