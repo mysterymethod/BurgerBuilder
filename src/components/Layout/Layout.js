1d83d1b109aaf04b7834db2c1e1f2d414109438a
mysterymethod <chandra.pranay@yahoo.in>
2020-09-14 14:21:51 +0530

    Setup the project file

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Aux from '../../hoc/Auxiliary';
+
+const layout = (props) => {
+    return (
+        <Aux>
+            <div>
+                Toobar, SideDrawer, Backdrop
+            </div>
+
+            <main>
+                {props.children}
+            </main>
+        </Aux>
+    );
+};
+
+export default layout;
